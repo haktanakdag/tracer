d33ab041ba6930f161e13ef0157f93d2f9826f3e
Haktan <haktan.akdag@gmail.com>
2018-03-06 23:18:05 +0300

    Add files via upload

---
+++ TracertRunner_v1.01/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Collections.Generic
+Imports System.IO
+Imports System.Timers
+Module Module1
+    Dim aTimer As New System.Timers.Timer
+    Dim p() As Process
+    Private eventlog As List(Of String)
+    Private nEventsFired As Integer = 0
+    Sub Main()
+        eventlog = New List(Of String)()
+        aTimer.AutoReset = True
+        aTimer.Interval = 2000 '2 seconds
+        AddHandler aTimer.Elapsed, AddressOf tick
+        aTimer.Start()
+        Console.WriteLine("Press the Enter key to exit the program... ")
+        Console.ReadLine()
+        For Each item In eventlog
+            Console.WriteLine(item)
+        Next
+        Console.WriteLine("Terminating the application...")
+    End Sub
+    Private Sub tick(ByVal sender As Object, ByVal e As System.Timers.ElapsedEventArgs)
+        p = Process.GetProcessesByName("TRACERT")
+        If p.Count < 1 Then
+            Console.WriteLine(" TRACERT Çalıştı..")
+            Process.Start("C:\sysnet.cmd")
+        End If
+    End Sub
+End Module
---
+++ TracertRunner_v1.01/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TracertRunner_v1.01")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TracertRunner_v1.01")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f78e78a5-0c10-4cf3-9087-66f761bd179d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
